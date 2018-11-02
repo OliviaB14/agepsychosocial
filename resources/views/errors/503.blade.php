e368f6a4fa3119133b4d826d473643aa254a8df5
olivia <olivia.boumokonia@laposte.net>
2018-11-02 01:29:24 +0100

    error handling 2

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.error')
+
+@section('code')
+    503
+@endsection
+@section('error_title')
+    Site en maintenance
+@endsection
+@section('error_message')
+    Le site est en maintenance, actualisez la page dans quelques minutes.
+@endsection
